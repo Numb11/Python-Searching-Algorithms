da914097ba9405555ec2ad1a25fd0fa57bbd3a4a
Soup <87945079+Numb11@users.noreply.github.com>
2022-05-17 23:29:39 +0100

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,28 +1,12 @@
-def types(array):
-   array = list(map(type,array))
-   if len(array) == array.count(array[0]):
-      return True
-   else:
-      return False
-
-class arrays:
-   def __init__(self, length,array,typey):
-          self.length = length
-          self.type = typey
-          if types(array) == True:
-            self.actarray = array
-          else:
-            print("Type error, input values do not match")
-            exit()
-
-   def appends(self,item):
-         if len(self.actarray) < self.length and isinstance(item,self.type):
-             self.actarray.append(item)
-
-   def array(self):
-       print(self.actarray)
-
-
-p1 = arrays(3,[1,2],int)
-p1.appends("cheese")
-p1.array()
+def binarysearchint (array,find):
+       found = False
+       index = 0
+       while found != True:
+           if array[(len(array))//2] == find:
+               found = True
+           elif array[(len(array))//2] > find:
+               array = array[:len(array)//2]
+               index = index + 1
+           elif array[(len(array))//2] < find:
+               array = array[len(array)//2:]
+           print(array)
