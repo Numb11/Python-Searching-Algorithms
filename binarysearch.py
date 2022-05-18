e9a0a6b928533f45601ff44fc55a7066ab3ab8f0
Soup <87945079+Numb11@users.noreply.github.com>
2022-05-18 17:05:04 +0100

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,12 +1,16 @@
 def binarysearchint (array,find):
        found = False
-       index = 0
-       while found != True:
-           if array[(len(array))//2] == find:
-               found = True
-           elif array[(len(array))//2] > find:
-               array = array[:len(array)//2]
-               index = index + 1
-           elif array[(len(array))//2] < find:
-               array = array[len(array)//2:]
-           print(array)
+       mid = 0
+       index = -1
+       low = 0
+       high = len(array) -1
+       while low <= high and not found:
+           mid = (high + low)//2
+           if array[mid] == find:
+               return mid
+           elif array[mid] < find:
+               low = mid + 1
+           elif array[mid] > find:
+               high = mid - 1
+       return index
+print(binarysearchint([2,31,40,44,52,56,57,60],60))
