3ff9abc2b0e35df3200a70d6e5eff31e3ca562d7
Soup <87945079+Numb11@users.noreply.github.com>
2022-05-23 13:16:01 +0100

    Create Merge.py

---
+++ Merge.py
@@ -0,0 +1,31 @@
+def mergesort (array):
+ left = []
+ right = []
+ if len(array)> 1:
+    mid = len(array)//2
+    left = array[:mid]
+    right = array[mid:]
+    mergesort(left)
+    mergesort(right)
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left) and j < len(right):
+     if left[i] < right [j]:
+         array[k] = left[i]
+         i = i+1
+     else:
+         array[k] = right[j]
+         j = j + 1
+     k = k + 1
+
+    while i < len(left):
+         array[k] = left[i]
+         i = i+ 1
+         k = k + 1
+
+    while j < len(right):
+         array[k] = right[j]
+         j = j + 1
+         k = k + 1
+ return array
